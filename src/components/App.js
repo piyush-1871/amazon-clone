c629afd013a12321b36d665e74d4b920b714c702
piyush-1871 <piyush1871b@gmail.com>
2022-10-19 13:45:17 +0530

    Banner implemented with styles

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Header from './Header';
+import Home from './Home';
 function App() {
   return (
     <Router>
       <div className="app">
         <Routes>
-          <Route path="/checkout" element={<h1>Checkout Page</h1>}></Route>
+          <Route path="/checkout" element={<><Header/><h1>Checkout</h1></>}></Route>
           <Route path="/login" element={<h1>Login</h1>}></Route>
           {/* This is default route */}
-          <Route path="/" element={<Header />}></Route>
+          <Route path="/" element={<><Header /><Home /></>}></Route>
         </Routes>
       </div>
     </Router>
