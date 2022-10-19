433c37e9b2a364f0299ca4145650ed855df652c0
piyush-1871 <piyush1871b@gmail.com>
2022-10-19 15:02:21 +0530

    implemented Product component with styles

---
+++ src/components/Product.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Product.css';
+
+function Product({ id, title, image, price, rating }) {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>Rs. </small>
+          <strong>{price}</strong>
+        </p>
+      </div>
+
+      <div className="product__rating">
+        {Array(rating)
+          .fill()
+          .map((_) => (
+            <p>⭐</p>
+          ))}
+      </div>
+      <img src={image} alt="" />
+      <button>Add to Cart</button>
+    </div>
+  );
+}
+
+export default Product;
