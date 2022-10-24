c6769317816e7a32a3b54c76d79fa3048986ebdc
piyush-1871 <piyush1871b@gmail.com>
2022-10-24 18:50:07 +0530

    build checkout page

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -1,9 +1,34 @@
 import React from 'react'
 import './Subtotal.css'
+import {useStateValue} from './StateProvider';
+import { getBasketTotal } from './reducer';
+import CurrencyFormat from 'react-currency-format';
 function Subtotal() {
+    const [{ basket }, dispatch] = useStateValue();
   return (
     <div className='subtotal'>
         {/* price */}
+        <CurrencyFormat
+        renderText={(value) => (
+            <>
+            <p>
+                Subtotal ({basket?.length} items): 
+                <strong>
+                    {value}
+                </strong>
+            </p>
+            <small className='subtotal__gift'>
+                <input type='checkbox' />
+                This order contains gift.
+            </small>
+            </>
+        )}
+            decimalScale={2}
+            value={getBasketTotal(basket)}
+            displayType={'text'}
+            thousandSeparator={true}
+            prefix={'Rs.'}
+        />
         <button>Proceed to Checkout</button>
     </div>
   )
